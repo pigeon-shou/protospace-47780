c28a821b97e29f9b92b4ffb8b1bef527cd2ac162
pigeon-shou <pigeonshow2024@gmail.com>
2026-04-09 18:32:54 +0900

    before_action :set_prototypeの追加　privateメソッド内の整形　render-buildの差し戻し

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,8 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   # ログインの有無のみならず、プロトタイプの投稿者かどうか判別する
-  def move_to_index
-  redirect_to root_path unless current_user == @prototype.user
-  end
 
   def index
     # データとしては配列
@@ -36,31 +34,36 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    # すでに存在しているレコードを選択して中身を書き換える。renderするなら@使え
-    @prototype = Prototype.find(params[:id])
+    # すでに存在しているレコードを選択して中身を書き換える。
     # updateメソッドにはsaveが含まれている。既に画像はレコードにあるのでpresence :falseでも通る
     if @prototype.update(prototype_params)
       redirect_to root_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
 
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
 
   private
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  
+  def move_to_index
+  redirect_to root_path unless current_user == @prototype.user
+  end
+
+  def set_prototype
+  @prototype = Prototype.find(params[:id])
+  end
+
 end
